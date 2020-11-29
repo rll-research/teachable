be81e3712ca542041d90623644a46343db51096d
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-11-29 14:36:53 -0800

    steeper intermediate reward scheme

--- babyai/levels/teachable_robot_levels.py
+++ babyai/levels/teachable_robot_levels.py
@@ -432,10 +432,10 @@ class Level_TeachableRobot(RoomGridLevel, MetaEnv):
         else:
             info['teacher_action'] = np.array([self.action_space.n], dtype=np.int32)
         # Reward at the end scaled by 1000
-        reward_total = np.ceil(rew) * 10
+        reward_total = rew * 1000
         if self.intermediate_reward:
             reward_total += int(give_reward)
-        rew = reward_total / 10
+        rew = reward_total / 1000
         return obs, rew, done, info
 
     def compute_give_reward(self, action):  # TODO: consider computing dense rewards as a dictionary too
