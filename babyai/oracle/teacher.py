e9324438e17e8a924a0bcedcaebda5f9699e3dd4
AliengirlLiv <oliviagracewatkins@gmail.com>
2020-12-22 00:09:39 -0800

    hopefully fix teacher errors

--- babyai/oracle/teacher.py
+++ babyai/oracle/teacher.py
@@ -77,19 +77,21 @@ class Teacher:
     def step_away_state(self, env_copy, oracle, steps):
         next_action = -1
         for step in range(steps):
-            vis_mask = oracle.vis_mask
-            new_oracle = self.botclass(oracle.mission)
+            vis_mask = oracle.vis_mask.copy()
+            new_oracle = self.botclass(env_copy)
             drop_off = len(oracle.stack) > 0 and oracle.mission.carrying and oracle.stack[-1].reason == 'DropOff' and \
                        (not next_action == oracle.mission.actions.toggle)
             if drop_off:
-                next_action, next_subgoal = oracle.replan(next_action)
+                next_action, next_subgoal = copy.deepcopy(oracle).replan(next_action)
             else:
                 new_oracle.vis_mask = vis_mask
                 new_oracle.step = oracle.step
+                new_oracle._process_obs()
                 next_action, next_subgoal = new_oracle.replan(-1)
                 oracle = new_oracle
             env_copy.teacher = None
-            if next_action == -1:  # TODO: Is this ever triggered?  As far as I can tell, no.
+            if next_action == -1:
+                assert False, "It was triggered after all"
                 next_state = env_copy.gen_obs()['obs']
             else:
                 next_state,  rew,  done,  info = env_copy.step(next_action)
