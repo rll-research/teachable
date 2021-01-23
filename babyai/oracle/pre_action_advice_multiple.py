f77a30507356ba346592f66f191e8255eee085aa
AliengirlLiv <oliviagracewatkins@gmail.com>
2021-01-23 00:31:46 -0800

    once again, version of PA3 which doesn't require memory

--- babyai/oracle/pre_action_advice_multiple.py
+++ babyai/oracle/pre_action_advice_multiple.py
@@ -14,10 +14,10 @@ class PreActionAdviceMultiple(Teacher):
         """
         Return a tensor corresponding to no feedback.
         """
-        return np.concatenate([self.one_hotify(-1) for action in self.action_list])
+        # return np.concatenate([self.one_hotify(-1) for action in self.action_list])
         # return np.concatenate([self.one_hotify(action) for action in self.action_list] + [np.array([self.steps_since_lastfeedback])])
-        # action = -1 if self.steps_since_lastfeedback in [-1, None] else self.action_list[self.steps_since_lastfeedback]
-        # return np.concatenate([self.one_hotify(action), np.array([self.steps_since_lastfeedback])])
+        action = -1 if self.steps_since_lastfeedback in [-1, None] else self.action_list[self.steps_since_lastfeedback]
+        return np.concatenate([self.one_hotify(action), np.array([self.steps_since_lastfeedback])])
 
     def random_feedback(self):
         """
@@ -33,10 +33,10 @@ class PreActionAdviceMultiple(Teacher):
         # Copy so we don't mess up the state of the real oracle
         oracle_copy = pkl.loads(pkl.dumps(oracle))
         self.step_ahead(oracle_copy, last_action=last_action)
-        return np.concatenate([self.one_hotify(action) for action in self.action_list])
+        # return np.concatenate([self.one_hotify(action) for action in self.action_list])
         # return np.concatenate([self.one_hotify(action) for action in self.action_list] + [np.array([self.steps_since_lastfeedback])])
-        # action = -1 if self.steps_since_lastfeedback in [-1, None] else self.action_list[self.steps_since_lastfeedback]
-        # return np.concatenate([self.one_hotify(action), np.array([self.steps_since_lastfeedback])])
+        action = -1 if self.steps_since_lastfeedback in [-1, None] else self.action_list[self.steps_since_lastfeedback]
+        return np.concatenate([self.one_hotify(action), np.array([self.steps_since_lastfeedback])])
 
     def one_hotify(self, index):
         correction = np.zeros((self.action_space.n + 1,))
