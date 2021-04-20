7c227b82fbb60865f872a0b36df97edbbed9993c
AliengirlLiv <oliviagracewatkins@gmail.com>
2021-04-19 21:14:13 -0700

    prevent crashes

--- d4rl/oracle/waypoint_teacher.py
+++ d4rl/oracle/waypoint_teacher.py
@@ -25,19 +25,24 @@ class WaypointCorrections(Teacher):
         Return the expert action from the previous timestep.
         """
         agent_pos = env.get_pos()
-        dist_to_curr_waypoint = np.linalg.norm(agent_pos - self.static_waypoints[0])
-        if len(self.waypoints) > 1:
-            dist_to_future_waypoint = np.linalg.norm(agent_pos - self.static_waypoints[1])
-        else:
-            dist_to_future_waypoint = float('inf')
-        close_to_curr_waypoint = dist_to_curr_waypoint < .25
-        closer_to_next_waypoint = dist_to_future_waypoint < dist_to_curr_waypoint
-        if close_to_curr_waypoint or closer_to_next_waypoint:
-            self.static_waypoints = self.static_waypoints[1:]
-            gave_feedback = True
-        else:
+        if len(self.static_waypoints) == 0:  # already succeeded
+            waypoint = np.array(env.get_target())
             gave_feedback = False
-        return self.static_waypoints[0].copy(), gave_feedback
+        else:
+            dist_to_curr_waypoint = np.linalg.norm(agent_pos - self.static_waypoints[0])
+            if len(self.waypoints) > 1:
+                dist_to_future_waypoint = np.linalg.norm(agent_pos - self.static_waypoints[1])
+            else:
+                dist_to_future_waypoint = float('inf')
+            close_to_curr_waypoint = dist_to_curr_waypoint < .25
+            closer_to_next_waypoint = dist_to_future_waypoint < dist_to_curr_waypoint
+            if close_to_curr_waypoint or closer_to_next_waypoint:
+                self.static_waypoints = self.static_waypoints[1:]
+                gave_feedback = True
+            else:
+                gave_feedback = False
+            waypoint = self.static_waypoints[0].copy()
+        return waypoint, gave_feedback
 
     # TODO: THIS IS NO IMPLEMENTED FOR THIS TEACHER! IF WE END UP USING THIS METRIC, WE SHOULD MAKE IT CORRECT!
     def success_check(self, state, action, oracle):
