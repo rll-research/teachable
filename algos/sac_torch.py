37767c58d595643b84daeb371e89a47d784f7663
AliengirlLiv <oliviagracewatkins@gmail.com>
2021-07-26 13:47:40 -0700

    add babyai conv

--- algos/sac_torch.py
+++ algos/sac_torch.py
@@ -13,6 +13,59 @@ from logger import logger
 from algos import utils
 
 
+# From https://github.com/ikostrikov/pytorch-a2c-ppo-acktr/blob/master/model.py
+def initialize_parameters(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        m.weight.data.normal_(0, 1)
+        m.weight.data *= 1 / torch.sqrt(m.weight.data.pow(2).sum(1, keepdim=True))
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+
+
+# Inspired by FiLMedBlock from https://arxiv.org/abs/1709.07871
+class FiLM(nn.Module):
+    def __init__(self, in_features, out_features, in_channels, imm_channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels, out_channels=imm_channels,
+            kernel_size=(3, 3), padding=1)
+        self.bn1 = nn.BatchNorm2d(imm_channels)
+        self.conv2 = nn.Conv2d(
+            in_channels=imm_channels, out_channels=out_features,
+            kernel_size=(3, 3), padding=1)
+        self.bn2 = nn.BatchNorm2d(out_features)
+
+        self.weight = nn.Linear(in_features, out_features)
+        self.bias = nn.Linear(in_features, out_features)
+
+        self.apply(initialize_parameters)
+
+    def forward(self, x, y):
+        x = F.relu(self.bn1(self.conv1(x)))
+        # x = F.relu(self.conv1(x))
+        x = self.conv2(x)
+        weight = self.weight(y).unsqueeze(2).unsqueeze(3)
+        bias = self.bias(y).unsqueeze(2).unsqueeze(3)
+        out = x * weight + bias
+        return F.relu(self.bn2(out))
+        # return F.relu(out)
+
+
+class ImageBOWEmbedding(nn.Module):
+    def __init__(self, max_value, embedding_dim):
+        super().__init__()
+        self.max_value = max_value
+        self.embedding_dim = embedding_dim
+        self.embedding = nn.Embedding(3 * max_value, embedding_dim)
+        self.apply(initialize_parameters)
+
+    def forward(self, inputs):
+        offsets = torch.Tensor([0, self.max_value, 2 * self.max_value]).to(inputs.device)
+        inputs = (inputs + offsets[None, :, None, None]).long()
+        return self.embedding(inputs).sum(1).permute(0, 3, 1, 2)
+
+
 class TanhTransform(pyd.transforms.Transform):
     domain = pyd.constraints.real
     codomain = pyd.constraints.interval(-1.0, 1.0)
@@ -86,8 +139,7 @@ class DiagGaussianActor(nn.Module):
             # constrain log_std inside [log_std_min, log_std_max]
             log_std = torch.tanh(log_std)
             log_std_min, log_std_max = self.log_std_bounds
-            log_std = log_std_min + 0.5 * (log_std_max - log_std_min) * (log_std +
-                                                                         1)
+            log_std = log_std_min + 0.5 * (log_std_max - log_std_min) * (log_std + 1)
 
             std = log_std.exp()
 
@@ -124,6 +176,62 @@ class DoubleQCritic(nn.Module):
         return q1, q2
 
 
+class InstrEmbedding(nn.Module):
+    def __init__(self, args, env):
+        # Define instruction embedding
+        super().__init__()
+        self.word_embedding = nn.Embedding(len(env.vocab()), args.instr_dim)
+        self.instr_rnn = nn.GRU(args.instr_dim, args.instr_dim, batch_first=True, bidirectional=False)
+
+        self.controllers = []
+        for i in range(args.num_modules):
+            mod = FiLM(
+                in_features=args.instr_dim,
+                out_features=128 if i < args.num_modules - 1 else args.image_dim,
+                in_channels=128, imm_channels=128)
+            self.controllers.append(mod)
+            self.add_module('FiLM_' + str(i), mod)
+
+    def forward(self, obs):
+        instruction_vector = obs.instr.long()
+        instr_embedding = self._get_instr_embedding(instruction_vector)
+        obs.instr = instr_embedding
+        return obs
+
+    def _get_instr_embedding(self, instr):
+        lengths = (instr != 0).sum(1).long()
+        out, _ = self.instr_rnn(self.word_embedding(instr))
+        hidden = out[range(len(lengths)), lengths - 1, :]
+        return hidden
+
+
+class ImageEmbedding(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.image_conv = nn.Sequential(*[
+            ImageBOWEmbedding(147, 128),
+            nn.Conv2d(
+                in_channels=128, out_channels=32,
+                kernel_size=(8, 8), stride=8, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+            nn.Conv2d(in_channels=32, out_channels=128, kernel_size=(3, 3), padding=1, stride=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2),
+        ])
+        self.film_pool = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+
+        # Initialize parameters correctly
+        self.apply(initialize_parameters)
+
+    def forward(self, obs):
+        inputs = torch.transpose(torch.transpose(obs.obs, 1, 3), 2, 3)
+        obs.obs = self.image_conv(inputs).flatten(1)
+        return obs
+
+
 class SACAgent:
     """SAC algorithm."""
 
@@ -135,7 +243,6 @@ class SACAgent:
                  batch_size=1024, learnable_temperature=True, control_penalty=0, repeat_advice=1):
         super().__init__()
         obs = env.reset()
-        obs_dim = len(obs['obs'].flatten()) + len(obs[teacher]) * repeat_advice
         if args.discrete:
             action_dim = env.action_space.n
         else:
@@ -156,6 +263,18 @@ class SACAgent:
         self.control_penalty = control_penalty
         self.repeat_advice = repeat_advice
 
+        if args.image_obs:
+            self.image_encoder = ImageEmbedding().to(self.device)
+            obs_dim = args.image_dim + len(obs[teacher]) * repeat_advice
+        else:
+            self.image_encoder = None
+            obs_dim = len(obs['obs'].flatten()) + len(obs[teacher]) * repeat_advice
+
+        if not args.no_instr:
+            self.instr_encoder = InstrEmbedding(args, env).to(self.device)
+            obs_dim = args.image_dim + len(obs[teacher]) * repeat_advice
+        else:
+            self.instr_encoder = None
         self.critic = DoubleQCritic(obs_dim, action_dim, hidden_dim=args.hidden_size).to(self.device)
         self.critic_target = DoubleQCritic(obs_dim, action_dim, hidden_dim=args.hidden_size).to(self.device)
         self.critic_target.load_state_dict(self.critic.state_dict())
@@ -194,6 +313,13 @@ class SACAgent:
         return self.log_alpha.exp()
 
     def act(self, obs, sample=False):
+        obs0 = obs
+        if self.image_encoder is not None:
+            obs = self.image_encoder(obs)
+            obs1 = obs
+        if self.instr_encoder is not None:
+            obs = self.instr_encoder(obs)
+            obs2 = obs
         obs = torch.cat([obs.obs.flatten(1)] + [obs.advice] * self.repeat_advice, dim=1).to(self.device)
         dist = self.actor(obs)
         if self.args.discrete:
@@ -298,12 +424,17 @@ class SACAgent:
             next_obs = val_batch.next_obs
             not_done = 1 - val_batch.full_done.unsqueeze(1)
             obs = self.obs_preprocessor(obs, self.teacher, show_instrs=True)
+            if self.image_encoder is not None:
+                obs = self.image_encoder(obs)
+                next_obs = self.image_encoder(next_obs)
+            if self.instr_encoder is not None:
+                obs = self.instr_encoder(obs)
+                next_obs = self.instr_encoder(next_obs)
             obs = torch.cat([obs.obs.flatten(1)] + [obs.advice] * self.repeat_advice, dim=1).to(self.device)
             next_obs = self.obs_preprocessor(next_obs, self.teacher, show_instrs=True)
             next_obs = torch.cat([next_obs.obs.flatten(1)] + [next_obs.advice] * self.repeat_advice, dim=1).to(
                 self.device)
             self.update_critic(obs, action, reward, next_obs, not_done, train=False)
-
 
     def optimize_policy(self, batch, step):
         obs = batch.obs
@@ -314,6 +445,12 @@ class SACAgent:
         next_obs = batch.next_obs
         not_done = 1 - batch.full_done.unsqueeze(1)
         obs = self.obs_preprocessor(obs, self.teacher, show_instrs=True)
+        if self.image_encoder is not None:
+            obs = self.image_encoder(obs)
+            next_obs = self.image_encoder(next_obs)
+        if self.instr_encoder is not None:
+            obs = self.instr_encoder(obs)
+            next_obs = self.instr_encoder(next_obs)
         obs = torch.cat([obs.obs.flatten(1)] + [obs.advice] * self.repeat_advice, dim=1).to(self.device)
         next_obs = self.obs_preprocessor(next_obs, self.teacher, show_instrs=True)
         next_obs = torch.cat([next_obs.obs.flatten(1)] + [next_obs.advice] * self.repeat_advice, dim=1).to(self.device)
@@ -410,7 +547,7 @@ class SACAgent:
         # entropy = dist.entropy().mean()
         entropy = 0
         # TODO: consider re-adding recon loss
-        loss = policy_loss# - self.args.entropy_coef * entropy
+        loss = policy_loss  # - self.args.entropy_coef * entropy
 
         ### LOGGING ###
         log = self.log_distill(action_true, action_teacher, entropy, policy_loss, loss, dist, is_training)
