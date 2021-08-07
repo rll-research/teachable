2f551f4a6c8cdacbb4f9b409e52410aa11a33abf
AliengirlLiv <oliviagracewatkins@gmail.com>
2021-08-06 21:30:43 -0700

    add relabel flag to test_generalization

--- scripts/test_generalization.py
+++ scripts/test_generalization.py
@@ -427,6 +427,7 @@ def main():
     parser.add_argument('--early_stop', type=int, default=None)
     parser.add_argument('--early_stop_metric', type=str, default=None)
     parser.add_argument('--distill_dropout_prob', type=float, default=.5)
+    parser.add_argument('--relabel', action='store_true')
     args = parser.parse_args()
     set_seed(args.seeds[0])
 
@@ -548,6 +549,7 @@ def main():
     additional_args['early_stop'] = args.early_stop
     additional_args['early_stop_metric'] = args.early_stop_metric
     additional_args['distill_dropout_prob'] = args.distill_dropout_prob
+    additional_args['relabel'] = args.relabel
     if args.collect_with_oracle:
         additional_args['source'] = 'teacher'
     if args.buffer_name is not None:
