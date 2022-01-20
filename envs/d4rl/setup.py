4277bf1eaf48267b387cf3ec78a07e3aac19c99f
AliengirlLiv <oliviagracewatkins@gmail.com>
2022-01-20 19:32:48 +0000

    remove unnecessary packages from setup.py

--- envs/d4rl/setup.py
+++ envs/d4rl/setup.py
@@ -8,19 +8,9 @@ setup(
                       'numpy', 
                       'mujoco_py', 
                       'pybullet',
-                      'h5py', 
-                      'termcolor', # adept_envs dependency
-                      'click',  # adept_envs dependency
-                      'dm_control @ git+git://github.com/deepmind/dm_control@master#egg=dm_control',
-                      'mjrl @ git+git://github.com/aravindr93/mjrl@master#egg=mjrl'],
+                      'h5py',], 
     packages=find_packages(),
     package_data={'d4rl_content': ['locomotion/assets/*',
-                           'hand_manipulation_suite/assets/*',
-                           'hand_manipulation_suite/Adroit/*',
-                           'hand_manipulation_suite/Adroit/gallery/*',
-                           'hand_manipulation_suite/Adroit/resources/*',
-                           'hand_manipulation_suite/Adroit/resources/meshes/*',
-                           'hand_manipulation_suite/Adroit/resources/textures/*',
                            ]},
     include_package_data=True,
 )
