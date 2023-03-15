e73d5d3bf82fe55ef366478d879c2e2dc9d7c959
Claire Sturgill <claire.sturgill@bccn-berlin.de>
2023-03-16 00:32:17 +0100

    Waypoint to Directional advice converter

--- utils/test_creating_graphs.py
+++ utils/test_creating_graphs.py
@@ -1,17 +1,27 @@
 import unittest
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 class MyTestCase(unittest.TestCase):
 
 
     def test_something(self):
-        data = pd.read_csv("/Users/1000ber-5078/PycharmProjects/teachable-rl/scripts/logs/persisted_models_distill/claire/results.csv")
+        grounding = pd.read_csv("/home/claire/Projects_Test/teachable-rl/scripts/logs/persisted_models_distill/claire/results.csv")
+        mlp_distill = pd.read_csv("/home/claire/Projects_Test/teachable-rl/scripts/logs/persisted_models_distill/distilled_with_mlp_2/results.csv")
+        paper_distill = pd.read_csv("/home/claire/Projects_Test/teachable-rl/scripts/logs/persisted_models_distill/original_distillation/results.csv")
 
+        mlp_distill = pd.concat([grounding, mlp_distill])
+        paper_distill = pd.concat([grounding, paper_distill])
 
-        plt.plot(data['itr'], data['reward'])
+        plt.plot(range(0, paper_distill.shape[0]*20, 20), paper_distill['reward'], label="original distillation")
+        plt.plot(range(0, mlp_distill.shape[0]*20, 20), mlp_distill['reward'], label="our version")
+        plt.xlabel("# iterations")
+        plt.ylabel("reward")
+        plt.title("reward over time for different distillation methods")
+        plt.legend()
         plt.show()
-        self.assertEqual(True, False)  # add assertion here
+        #self.assertEqual(True, False)  # add assertion here
 
 
 if __name__ == '__main__':
